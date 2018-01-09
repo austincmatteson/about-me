90fdcbb918e14ca82d3ce4fdbf78f5b6c6365f2f
austincmatteson <austin.c.matteson@gmail.com>
2018-01-09 13:51:44 -0800

    initial setup of css, javascript, and lintr files

---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// var userName = prompt('Hi, What is your name?');
+
+// var pets = parseInt(prompt('How many pets do you think I have?'));
+
+//PSEUDOCODE
+// if(user gues the correct number) {
+//   tell the user they were correct
+// } else {
+//   tell the user they were wrong
+// }
+
+// if(pets === 2){
+//   alert('That is correct!');
+// } else{
+//   alert('No, that is incorrect.');
+// }
+
+// Question about age
+// var alliesAge = prompt('Do you think I\'m older than 30? Answer with Y or N.').toUpperCase();
+
+// if(alliesAge === 'Y') {
+//   alert('Yes, I\'m older than 30.');
+// } else if(alliesAge === 'N') {
+//   alert('Thank you, I\'m flattered.');
+// } else {
+//   alert('Please enter Y or N');
+// }
+
+// var arrayOfDogs = ['Gary', 'Charlotte', 'Demi', 'Bird', 'Mr. Darcy'];
+// arrayOfDogs[5] = 'Whiskey';
+// arrayOfDogs[5] = 'Holly';
+// console.log(arrayOfDogs.length);
+// arrayOfDogs.push('Toto');
+// console.log(arrayOfDogs.length);
+// arrayOfDogs.pop();
+// arrayOfDogs.unshift('Holly);
+// arrayOfDogs.shift('Holly);
+// console.log(arrayOfDogs);
