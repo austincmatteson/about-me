fe389c850b250c276a7029e638f9a77293d093cc
austincmatteson <austin.c.matteson@gmail.com>
2018-01-09 20:11:31 -0800

    Finalize javascript output of how well the user did

--- js/app.js
+++ js/app.js
@@ -19,16 +19,18 @@ realAnswers = [];
 
 //start questions
 alert('Hey, welcome to my site. My name is Austin Matteson but you may already know that. In fact, let\'s find out just how much you do know about me with five yes or no questions.');
+
+//question 1
 while (i < 1) {
   currentAnswer = prompt('Was I born in Washington?');
   currentAnswer = currentAnswer.toLowerCase();
-  if (currentAnswer === 'yes') {
-    currentAnswer = 'y';
-  } else if (currentAnswer === 'no') {
-    currentAnswer = 'n';
+  if (currentAnswer === 'y') {
+    currentAnswer = 'yes';
+  } else if (currentAnswer === 'n') {
+    currentAnswer = 'no';
   }
   
-  if (currentAnswer === 'y' || currentAnswer === 'n') { 
+  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
@@ -37,7 +39,7 @@ while (i < 1) {
     alert('Please answer with a yes or no response.')
   }
 }
-if (allAnswers[0] === 'n') {
+if (allAnswers[0] === 'no') {
   alert('Correct!');
   realAnswers.push('Correct');
   console.log(realAnswers);
@@ -52,13 +54,13 @@ i--;
 while (i < 1) {
   currentAnswer = prompt('Have I been skydiving?');
   currentAnswer = currentAnswer.toLowerCase();
-  if (currentAnswer === 'yes') {
-    currentAnswer = 'y';
-  } else if (currentAnswer === 'no') {
-    currentAnswer = 'n';
+  if (currentAnswer === 'y') {
+    currentAnswer = 'yes';
+  } else if (currentAnswer === 'n') {
+    currentAnswer = 'no';
   }
   
-  if (currentAnswer === 'y' || currentAnswer === 'n') { 
+  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
@@ -67,7 +69,7 @@ while (i < 1) {
     alert('Please answer with a yes or no response.')
   }
 }
-if (allAnswers[1] === 'n') {
+if (allAnswers[1] === 'no') {
   alert('Correct!');
   realAnswers.push('Correct');
   console.log(realAnswers);
@@ -82,13 +84,13 @@ i--;
 while (i < 1) {
   currentAnswer = prompt('Did I actually fall through the ice of a frozen pond?');
   currentAnswer = currentAnswer.toLowerCase();
-  if (currentAnswer === 'yes') {
-    currentAnswer = 'y';
-  } else if (currentAnswer === 'no') {
-    currentAnswer = 'n';
+  if (currentAnswer === 'y') {
+    currentAnswer = 'yes';
+  } else if (currentAnswer === 'n') {
+    currentAnswer = 'no';
   }
   
-  if (currentAnswer === 'y' || currentAnswer === 'n') { 
+  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
@@ -97,7 +99,7 @@ while (i < 1) {
     alert('Please answer with a yes or no response.')
   }
 }
-if (allAnswers[2] === 'y') {
+if (allAnswers[2] === 'yes') {
   alert('Correct!');
   realAnswers.push('Correct');
   console.log(realAnswers);
@@ -112,13 +114,13 @@ i--;
 while (i < 1) {
   currentAnswer = prompt('Is Queens of the Stone Age my favorite band?');
   currentAnswer = currentAnswer.toLowerCase();
-  if (currentAnswer === 'yes') {
-    currentAnswer = 'y';
-  } else if (currentAnswer === 'no') {
-    currentAnswer = 'n';
+  if (currentAnswer === 'y') {
+    currentAnswer = 'yes';
+  } else if (currentAnswer === 'n') {
+    currentAnswer = 'no';
   }
   
-  if (currentAnswer === 'y' || currentAnswer === 'n') { 
+  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
@@ -127,7 +129,7 @@ while (i < 1) {
     alert('Please answer with a yes or no response.')
   }
 }
-if (allAnswers[3] === 'y') {
+if (allAnswers[3] === 'yes') {
   alert('Correct!');
   realAnswers.push('Correct');
   console.log(realAnswers);
@@ -142,13 +144,13 @@ i--;
 while (i < 1) {
   currentAnswer = prompt('Do I watch Real Housewives shows?');
   currentAnswer = currentAnswer.toLowerCase();
-  if (currentAnswer === 'yes') {
-    currentAnswer = 'y';
-  } else if (currentAnswer === 'no') {
-    currentAnswer = 'n';
+  if (currentAnswer === 'y') {
+    currentAnswer = 'yes';
+  } else if (currentAnswer === 'n') {
+    currentAnswer = 'no';
   }
   
-  if (currentAnswer === 'y' || currentAnswer === 'n') { 
+  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
@@ -157,7 +159,7 @@ while (i < 1) {
     alert('Please answer with a yes or no response.')
   }
 }
-if (allAnswers[4] === 'y') {
+if (allAnswers[4] === 'yes') {
   alert('Correct!');
   realAnswers.push('Correct');
   console.log(realAnswers);
@@ -167,3 +169,12 @@ if (allAnswers[4] === 'y') {
   console.log(realAnswers);
 }
 i--;
+
+//user scorecard
+var score = 0;
+for (; i < 5; i++) {
+  if (realAnswers[i] === 'Correct'){
+    score++;
+  }
+}
+alert('You\'re answers of ' + allAnswers + ' means you got ' + score + ' out of 5 right. Enjoy the site!');
