37062ccd29b62e58e850b37370a01f8da2c70830
austincmatteson <austin.c.matteson@gmail.com>
2018-01-10 22:49:31 -0800

    implementation of the sixth question, with 4 chances for a right answer

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-/* 
-define variables for use 
-alert that questions will start 
+/*
+define variables for use
+alert that questions will start
 ask question
 must receive yes or no response
 convert to checkable answer
@@ -12,13 +12,14 @@ reveal how they did
 */
 
 //define variables
-var userName, currentAnswer, allAnswers, realAnswers, i;
+var userName, currentAnswer, allAnswers, realAnswers, questionTries, i;
 i = 0;
+questionTries = 4;
 allAnswers = [];
 realAnswers = [];
 
 //start questions
-alert('Hey, welcome to my site. My name is Austin Matteson but you may already know that. In fact, let\'s find out just how much you do know about me with five yes or no questions.');
+alert('Hey, welcome to my site. My name is Austin Matteson but you may already know that. In fact, let\'s find out just how much you do know about me.');
 userName = prompt('But first, what is your name?');
 console.log(userName);
 alert('Great name, ' + userName + ', let\'s begin the test!');
@@ -32,14 +33,14 @@ while (i < 1) {
   } else if (currentAnswer === 'n') {
     currentAnswer = 'no';
   }
-  
-  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
+
+  if (currentAnswer === 'yes' || currentAnswer === 'no') {
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
     i++;
   } else {
-    alert('Please answer with a yes or no response.')
+    alert('Please answer with a yes or no response.');
   }
 }
 if (allAnswers[0] === 'no') {
@@ -62,14 +63,14 @@ while (i < 1) {
   } else if (currentAnswer === 'n') {
     currentAnswer = 'no';
   }
-  
-  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
+
+  if (currentAnswer === 'yes' || currentAnswer === 'no') {
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
     i++;
   } else {
-    alert('Please answer with a yes or no response.')
+    alert('Please answer with a yes or no response.');
   }
 }
 if (allAnswers[1] === 'no') {
@@ -92,14 +93,14 @@ while (i < 1) {
   } else if (currentAnswer === 'n') {
     currentAnswer = 'no';
   }
-  
-  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
+
+  if (currentAnswer === 'yes' || currentAnswer === 'no') {
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
     i++;
   } else {
-    alert('Please answer with a yes or no response.')
+    alert('Please answer with a yes or no response.');
   }
 }
 if (allAnswers[2] === 'yes') {
@@ -122,14 +123,14 @@ while (i < 1) {
   } else if (currentAnswer === 'n') {
     currentAnswer = 'no';
   }
-  
-  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
+
+  if (currentAnswer === 'yes' || currentAnswer === 'no') {
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
     i++;
   } else {
-    alert('Please answer with a yes or no response.')
+    alert('Please answer with a yes or no response.');
   }
 }
 if (allAnswers[3] === 'yes') {
@@ -152,14 +153,14 @@ while (i < 1) {
   } else if (currentAnswer === 'n') {
     currentAnswer = 'no';
   }
-  
-  if (currentAnswer === 'yes' || currentAnswer === 'no') { 
+
+  if (currentAnswer === 'yes' || currentAnswer === 'no') {
     console.log(currentAnswer);
     allAnswers.push(currentAnswer);
     console.log(allAnswers);
     i++;
   } else {
-    alert('Please answer with a yes or no response.')
+    alert('Please answer with a yes or no response.');
   }
 }
 if (allAnswers[4] === 'yes') {
@@ -173,11 +174,42 @@ if (allAnswers[4] === 'yes') {
 }
 i--;
 
+//question 6, multiple chance guess the number
+while (i < questionTries) {
+  currentAnswer = parseInt(prompt('Try and guess how many first cousins I have.'));
+  console.log(currentAnswer);
+  if (currentAnswer === 3) {
+    alert('Correct, 3. And it only took you ' + (i + 1) + ' attempt(s).');
+    allAnswers.push(currentAnswer);
+    console.log(allAnswers);
+    realAnswers.push('Correct');
+    console.log(realAnswers);
+    i = 0;
+    break;
+  } else if (currentAnswer > 3) {
+    i++;
+    alert('That is too high. You have ' + (questionTries - i) + ' attempt(s) left.');
+  } else if (currentAnswer < 3) {
+    i++;
+    alert('That is too low. You have ' + (questionTries - i) + ' attempt(s) left.');
+  } else {
+    i++;
+    alert('Answer with a number. You have ' + (questionTries - i) + ' attempt(s) left.');
+  }
+}
+if (i === questionTries) {
+  alert('Too many failed attempts. The correct answer was 3.');
+  realAnswers.push('Incorrect');
+  console.log(realAnswers);
+  allAnswers.push('not 3');
+  i = 0;
+}
+
 //user scorecard
 var score = 0;
-for (; i < 5; i++) {
+for (; i < realAnswers.length; i++) {
   if (realAnswers[i] === 'Correct'){
     score++;
   }
 }
-alert(userName + ', you\'re answers of ' + allAnswers + ' mean you got ' + score + ' out of 5 right. Enjoy the site!');
+alert(userName + ', you\'re answers (' + allAnswers + ') mean you got ' + score + ' out of 7 right. Enjoy the site!');
